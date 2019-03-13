09749c21bab0395f583c77c3df903200ce730a27
Sid Hackney <snackney@gmail.com>
2019-03-13 02:58:49 -0500

    At smaller resolutions, menu will toggle when clicking hamburger

--- src/App.js
+++ src/App.js
@@ -15,6 +15,13 @@ import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } 
 // import './fontawesome/css/font-awesome.min.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      menuOpen: false,
+      skin: 'default'
+    };
+  }
   componentDidMount() {
     Events.scrollEvent.register('begin', function(to, element) {
       console.log("begin", arguments);
@@ -33,40 +40,52 @@ class App extends Component {
     Events.scrollEvent.remove('end');
   };
 
-  state = {
-    skin: 'default'
+  toggleMenu () {
+    // console.log(this);
+    this.setState({menuOpen: !this.state.menuOpen});
+    const toggleMenuClass = this.state.menuOpen ? 'show' : 'hide';
+    console.log(toggleMenuClass);
+    return toggleMenuClass;
+    
   };
 
   render() {
     return (
       <div className="App">
         <div className="App-header--wrapper">
-          <div className="App-logo">
-            <img src={logo} alt="My logo: an outlined, teal silhouette of a sea turtle with the letters 'SH' superimposed." />
-          </div>
           <div className="App-nav">
-            <ul className="App-nav--wrapper">
-              <li className="App-nav--item">
-                <Link activeClass="active" to="home" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
-                  Home
-                </Link>
-              </li>
-              <li className="App-nav--item">
-                <Link activeClass="active" to="projects" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
-                  Projects
-                </Link>
-              </li>
-              <li className="App-nav--item">
-                <Link activeClass="active" to="resume" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
-                  Resume
-                </Link>
-              </li>
-              <li className="App-nav--item">
-                <Link activeClass="active" to="about" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
-                  About Me
-                </Link>
-              </li>
-            </ul>
+            <div className="App-nav--hamburger" onClick={this.toggleMenu.bind(this)}>
+              <div className="bar1" />
+              <div className="bar2" />
+              <div className="bar3" />
+            </div>
+            <div className="App-logo">
+              <img src={logo} alt="My logo: an outlined, teal silhouette of a sea turtle with the letters 'SH' superimposed." />
+            </div>
+            <div className={this.state.menuOpen ? 'show' : 'hide'}>
+              <ul className="App-nav--wrapper">
+                <li className="App-nav--item">
+                  <Link activeClass="active" to="home" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
+                    Home
+                  </Link>
+                </li>
+                <li className="App-nav--item">
+                  <Link activeClass="active" to="projects" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
+                    Projects
+                  </Link>
+                </li>
+                <li className="App-nav--item">
+                  <Link activeClass="active" to="resume" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
+                    Resume
+                  </Link>
+                </li>
+                <li className="App-nav--item">
+                  <Link activeClass="active" to="about" smooth={true} offset={-70} duration={500} onSetActive={this.handleSetActive}>
+                    About Me
+                  </Link>
+                </li>
+              </ul>
+            </div>
             <div className="App-header--links">
               <a href="https://github.com/practicalmess">
                 <img src={githublogo} alt="The circular logo for Github"/>
