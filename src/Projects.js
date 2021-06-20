738f945a08147ad893e5866ff32b120418d72f22
Sid Hackney <snackney@gmail.com>
2021-06-20 12:56:02 -0400

    Add pdp as project

--- src/Projects.js
+++ src/Projects.js
@@ -9,6 +9,7 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Link, Element , Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 import './styles/App.css';
+import pdp from './images/pdpscreenshot.png';
 import beatbox from './images/bbscreenshot-sq.png';
 import bwhovians from './images/bwscreenshot.png';
 
@@ -18,6 +19,21 @@ const Projects = (props) => {
     <Element name="projects" className="element">
       <div className="App-section">
         <h1 className="App-section--header">Projects</h1>
+        <div className="Project-card">
+          <div className="Project-card--column1">
+            <a href="https://invis.io/WSLJFVHMC7V#/304593239_main_Page">
+              <img src={pdp} alt="A screenshot of the Wayfair product details page."/>
+            </a>
+          </div>
+          <div className="Project-card--column2">
+            <a href="https://www.wayfair.com/furniture/pdp/loon-peak-urbana-1-drawer-iron-nightstand-w002783661.html">
+              <h2>Wayfair Product Details Page</h2>
+            </a>
+            <p>Since November 2019 I have worked on the dedicated product details page team at Wayfair, supporting or leading many projects - from deprecating the "mweb" version of the page in favor of a single responsive layout to converting from working in a monolith to owning a fully decoupled and self-deployed webpack app.</p>
+            <p>Examples of components I deployed include the cards beneath the "What We Offer" header and the review summary block containing the numerical average and histogram view of customer ratings.</p>
+            
+          </div>
+        </div>
         <div className="Project-card">
           <div className="Project-card--column1">
             <a href="https://invis.io/WSLJFVHMC7V#/304593239_main_Page">
