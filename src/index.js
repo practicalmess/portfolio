2982e606ef330c96a6e1bfb6ebc1f46067fecf97
Sidney Hackney <snackney@gmail.com>
2023-10-18 18:53:36 -0400

    Add hashrouter

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import "./index.css";
 import App from "./App";
 // import Home from 'Home.js';
 import registerServiceWorker from "./registerServiceWorker";
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
+import { HashRouter } from "react-router-dom";
+
+ReactDOM.render(
+  <HashRouter>
     <App />
-  </React.StrictMode>
+  </HashRouter>,
+  document.getElementById("root")
 );
 
 // ReactDOM.render(<App />, document.getElementById('root'));
