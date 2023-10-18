5b639f18d8692b52580521f82db4dbbd31e21db3
Sidney Hackney <snackney@gmail.com>
2023-10-18 19:04:57 -0400

    Try again

--- src/index.js
+++ src/index.js
@@ -4,14 +4,6 @@ import "./index.css";
 import App from "./App";
 // import Home from 'Home.js';
 import registerServiceWorker from "./registerServiceWorker";
-import { HashRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <HashRouter>
-    <App />
-  </HashRouter>,
-  document.getElementById("root")
-);
-
-// ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
